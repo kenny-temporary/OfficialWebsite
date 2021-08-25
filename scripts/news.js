e7166ac14f0737f53c2224b5251b08d4a67ee536
kenny <lishiwen@wclound.cn>
2021-08-26 00:08:22 +0800

    feat: 分页逻辑

---
+++ scripts/news.js
@@ -0,0 +1,107 @@
+const defaultTemplate = `
+            <div class="row mb-lg-4 mt-lg-4 mb-md-2 mt-md-2">
+              <div class="col-lg-5 col-md-12">
+                <img data-sizes="auto" data-src="./images/02.png" class="img-fluid safe-image lazyload" />
+              </div>
+              <div class="col-lg-7 col-md-12 p-3">
+                <div class="lead" style="font-weight: bold;">
+                  見出しや副見出し、表示見出しなどで装飾することで
+                </div>
+            	<div class="text-black-50 my-2 small">
+            		发布日期: {{date}} 
+					作者: {{name}}
+            	</div>
+                <p>見出しや副見出し、表示見出しなどで装飾することで、font sizeを変更できます。詳しい方法を解説しますので、一緒に見ていきましょう。</p>
+              </div>
+            </div>
+          `;
+
+function generateNewsDocumentFragment(
+    dataList = [],
+    options = {
+        classnames: "",
+        style: ""
+    }
+) {
+    const fragment = document.createDocumentFragment();
+	fragment.innerHTML = ""
+    dataList.forEach((data) => {
+        const newsItemNode = generateNewsItemNode(data, options);
+        fragment.appendChild(generateNewsItemNode(data, options));
+    });
+    return fragment;
+}
+
+function generateNewsItemNode(newsItem = {}, options = {}) {
+    const li = document.createElement("li");
+    if (options.classnames) {
+        injecteClassname(options.classnames, li);
+    }
+    li.style = options.style;
+    li.innerHTML = newsItemTemplate(newsItem);
+    return li;
+}
+
+function newsItemTemplate(data = {}) {
+    const sourceTemplate = data.tempalte || defaultTemplate;
+    return parseHTML(sourceTemplate).data(data);
+}
+
+function parseHTML(htmlString = "", options) {
+    return {
+        data(data) {
+            return htmlString.replace(/\{\{(.+?)\}\}/g, (...args) => {
+                return args[1].split(".").reduce((data, currentValue) => {
+                    return data[currentValue.trim()];
+                }, data);
+            });
+        },
+    };
+}
+
+function injecteClassname(classnames, target) {
+    let classList = "";
+
+    if (!classnames) {
+        return "";
+    }
+
+    if (Array.isArray(classnames)) {
+        classnames.forEach((classname) => (classList += ` ${classname}`));
+    }
+
+    if (typeof classnames === "string") {
+        classList += ` ${classnames}`;
+    }
+
+    target.classList = classList.trim();
+}
+
+function appendChildToContainer(HTMLConllection, target) {
+    target.appendChild(HTMLConllection);
+}
+
+function renderTo(container) {
+    if (!container) {
+        throw new Error("No container");
+    }
+
+    const targrtContainer = document.querySelector(container);
+
+    return {
+        setData(data = [], options = {}) {
+			options.type = "additional";
+            const newsFragment = generateNewsDocumentFragment(data, options);
+            appendChildToContainer(newsFragment, targrtContainer);
+        },
+
+		resetData(data = [], options = {}){
+			options.type = "reset";
+			const newsFragment = generateNewsDocumentFragment(data, options);
+			targrtContainer.innerHTML = '';
+			targrtContainer.appendChild(newsFragment);
+		}
+    }
+}
+
+
---
+++ scripts/newsEntry.js
@@ -0,0 +1,81 @@
+// ------------------------ 初始化数据 ----------------------
+const {
+	setData,
+	resetData
+} = renderTo(".news-container");
+
+window.onload = function() {
+	loadMore();
+}
+
+function loadMore(params = {}) {
+	request().then(res => {
+		const rewirted = res.map(res => {
+			return {
+				...res,
+				date: new Date().toLocaleDateString() + Math.random()
+			}
+		});
+
+		if (params.type === updateUIType.Reset) {
+			resetData(rewirted, {
+				classnames: "col-md-6 d-flex justify-content-between news-card",
+			});
+			return;
+		}
+		setData(rewirted, {
+			classnames: "col-md-6 d-flex justify-content-between news-card",
+		});
+	})
+}
+
+function request() {
+	return fetch("https://jsonplaceholder.typicode.com/users")
+		.then(res => res.json())
+}
+
+
+// ------------------------ 分页事件 ----------------------
+const pagination = new Pagination();
+pagination.setMaxPageSize(10);
+
+
+
+const previousFakeBtn = document.getElementById("previous-btn");
+const nextFakeBtn = document.getElementById("next-btn");
+
+function diasbleEleClick(target){
+	target.classList.add("disable-click");
+}
+
+function removeDisableClick(target){
+	target.classList.remove("disable-click");
+}
+
+previousFakeBtn.addEventListener('click', function() {
+	pagination.previous();
+});
+
+nextFakeBtn.addEventListener('click', function() {
+	pagination.next();
+});
+
+pagination.onPageSizeChange(function onchane(id) {
+	debugger
+	if(id <= 0){
+		diasbleEleClick(previousFakeBtn);
+		removeDisableClick(nextFakeBtn);
+	}
+	
+	if(id > 1 && id < 10) {
+		removeDisableClick(nextFakeBtn);
+		removeDisableClick(previousFakeBtn);
+	}
+
+	if(id >= 10){
+		diasbleEleClick(nextFakeBtn);
+		removeDisableClick(previousFakeBtn);
+	};
+
+	return true;
+});
